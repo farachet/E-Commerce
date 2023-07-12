26ab8f99dd0d4dfa7d4198c9da600670f0eb9403
Haithembenkemla <haithembenkemla@gmail.com>
2023-07-12 22:00:33 +0100

    front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Client from './componenets/Client/Client.jsx';
 import Navbar from './componenets/navbar/navbar';
 import AllProducts from './componenets/seller/AllProducts';
 import AddProduct from './componenets/seller/AddProduct';
+import AboutUs from './componenets/Aboutus/AboutUs.jsx'
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
       <Navbar/>
    
    
-
-    <Client />
+    <AboutUs/>
+    
     </div>
     
  
