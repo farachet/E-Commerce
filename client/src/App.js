24108331a77c62cf52910f552df2cd5644ff5bdb
samarzribi <samarzribi014@gmail.com>
2023-07-12 11:53:05 +0100

    navbar almost done and cards almost done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import Navbar from './componenets/navbar/navbar';
+import AllProducts from './componenets/seller/AllProducts';
+import AddProduct from './componenets/seller/AddProduct';
 
 function App() {
   return (
