59a48fe5e68fa3812d8e0efa970073cc2f9a93eb
Dhiabenhassine <Dhiabenhssin123@gmail.com>
2023-07-14 18:27:12 +0100

    ok

--- server/index.js
+++ server/index.js
@@ -5,22 +5,17 @@ const categoryRouter = require("./routes/categoryRoutes");
 const AdminRouter = require("./routes/admin");
 const cardsRouter = require("./routes/cardsRoutes");
 const clientRouter = require("./routes/clientRoutes");
-const postsRouter = require("./routes/postsRouter")
-const personalcollectionRouter =require("./routes/personnalcollRouters")
-
+const postsRouter = require("./routes/postsRouter");
+const personalcollectionRouter = require("./routes/personnalcollRouters");
 
 const sequelize = require("./database/configdb");
 const { Sequelize } = require("sequelize");
-const cors = require("cors")
-
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-
-
-
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,11 +24,11 @@ app.use(express.static(__dirname + "/../client/dist"));
 app.use("/api/seller", sellerRoute);
 app.use("/api/product", productRoute);
 app.use("/api/admin", AdminRouter);
-app.use("/api/cards",cardsRouter);
-app.use("/api/client",clientRouter);
-app.use("/api/posts",postsRouter);
-app.use("/api/personalcollection ",personalcollectionRouter);
-app.use("/api/category",categoryRouter);
+app.use("/api/cards", cardsRouter);
+app.use("/api/client", clientRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/personalcollection ", personalcollectionRouter);
+//app.use("/api/category",categoryRouter);
 
 sequelize.sync().then(() => console.log("database connected"));
 sequelize
--- server/routes/productsRouters.js
+++ server/routes/productsRouters.js
@@ -1,16 +1,16 @@
-const router=require("express").Router()
-const {AddProducts , getAllProducts , UpdateProduct, deleteAll, deleteById}=require("../controllers/productscontroller"
+const router = require("express").Router();
+const {
+  AddProducts,
+  getAllProducts,
+  UpdateProduct,
+  deleteAll,
+  deleteById,
+} = require("../controllers/productscontroller");
 
+router.post("/AddProduct", AddProducts);
+router.get("/getAll", getAllProducts);
+router.put("/edit/:id", UpdateProduct);
+router.delete("/deleteAll", deleteAll);
+router.delete("/deleteByid/:id", deleteById);
 
-
-
-
-
-router.post("/AddProduct",AddProducts )
-router.get("/getAll",getAllProducts )
-router.put("/edit/:id",UpdateProduct )
-router.delete("/deleteAll", deleteAll )
-router.delete("/deleteByid/:id", deleteById )
-
-
-module.exports=router
+module.exports = router;
